bdb9913a608d697a74302e0913975b34adfc5e57
ayenyein22 <ayenyeinsan36550@gmail.com>
2023-03-09 19:58:12 +0630

    update javascript

--- javascriptpages/index.js
+++ javascriptpages/index.js
@@ -1,12 +1,13 @@
-function downloadLink() {
-  let name = prompt("What is your name?");
-  let email = prompt("What's your email?");
-  name = name.toLowerCase().trim();
-  if (name === "may pho pwint" && email === "mayphyopwint6@gmail.com") {
-    document.querySelector(".certi-download").innerHTML =
-      "Okay I have confirmed that you are May Phyo, You can download your certificate now.";
+function checkAndDownload(event) {
+  event.preventDefault();
+  password = prompt("To verify you are May Phyo, Enter our friendship code.");
+  if ((password = "773823032003")) {
+    document.querySelector(".certi-Link").innerHTML = "Click To Download";
+    document.querySelector(".certi-Link").href =
+      "https://drive.google.com/file/d/1v35SmKYCJ6twqoy5hPF-20bYawNjFzw5/view?usp=share_link";
+    document.querySelector(".download-certi-para").innerHTML =
+      "Okay, you are May Phyo!!! Now click the link!!";
   }
 }
-
-let certiLink = document.querySelector(".certi-link");
-certiLink.addEventListener("click", downloadLink);
+let link = document.querySelector(".certi-link");
+link.addEventListener("click", checkAndDownload);
